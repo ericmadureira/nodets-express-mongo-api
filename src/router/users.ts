96cba01cf8100457f707100624341945dc1497c1
Eric Madureira <ericmadureira@users.noreply.github.com>
2023-04-12 17:00:19 -0300

    user update logic
    Adds controller logic and route for user info update.

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 
-import { deleteUser, getAllUsers } from '../controllers/users'
+import { deleteUser, getAllUsers, updateUser } from '../controllers/users'
 import { isAuthenticated, isOwner } from '../middlewares'
 
 export default (router: Router) => {
   router.get('/users', isAuthenticated, getAllUsers)
   // A ordem importa! isAuthenticated deve vir primeiro.
   router.delete('/users/:id', isAuthenticated, isOwner, deleteUser)
+  router.patch('/users/:id', isAuthenticated, isOwner, updateUser)
 }
