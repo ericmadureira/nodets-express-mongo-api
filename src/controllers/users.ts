38ef888aa6949b7ef00d6c1e19d21e2a4353ce99
Eric Madureira <ericmadureira@users.noreply.github.com>
2023-04-12 14:24:45 -0300

    User Controller
    Adds an User Controller with list and delete methods.

---
+++ src/controllers/users.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+
+import { deleteUserById, getUsers } from '../db/users'
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await getUsers()
+
+    return res.status(200).json(users)
+  } catch (error) {
+    console.log(error)
+    return res.sendStatus(400)
+  }
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+
+    const deletedUser = await deleteUserById(id)
+    return res.json(deletedUser)
+  } catch (error) {
+    console.log(error)
+    return res.sendStatus(400)
+  }
+}
