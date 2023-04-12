0aa090518729839bce3de57d8ca53f1491b0511d
Eric Madureira <ericmadureira@users.noreply.github.com>
2023-04-12 11:23:26 -0300

    login controller and route
    Adds controller and route for user login. It checks if there's an user, if pw is correct and sets cookie with session token.

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,38 @@ import express from 'express'
 import { createUser, getUserByEmail } from '../db/users'
 import { authentication, random } from '../helpers'
 
+// POST + body
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body
+
+    if(!email || !password) {
+      return res.sendStatus(400)
+    }
+
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
+    if (!user) {
+      return res.sendStatus(400)
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password)
+    if(user.authentication.password !== expectedHash){
+      return res.sendStatus(403)
+    }
+
+    const salt = random()
+    user.authentication.sessionToken = authentication(salt, user._id.toString())
+    await user.save()
+
+    res.cookie('ERIC-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/'})
+    return res.sendStatus(200).json(user).end()
+  } catch (error) {
+    console.log(`REGISTER ERROR: ${error}`)
+    return res.sendStatus(400)
+  }
+}
+
+// POST + body
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body
